8d4d0c0641e0bd0b5b1829e6e4cb84dc73712085
meinhere <sabilahmad615@gmail.com>
2022-11-30 12:14:44 +0700

    first commit

---
+++ kalkulator/assets/kalkulator.js
@@ -0,0 +1,112 @@
+const calculator = {
+  displayNumber: "0",
+  operator: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+  if (calculator.displayNumber == "0") {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
+}
+
+function inverseNumber() {
+  if (calculator.displayNumber == "0") {
+    return;
+  }
+  calculator.displayNumber *= -1;
+}
+
+function handleOperation(operator) {
+  if (!calculator.isWaitForSecondNumber) {
+    calculator.operator = operator;
+    calculator.isWaitForSecondNumber = true;
+    calculator.firstNumber = calculator.displayNumber;
+
+    calculator.displayNumber = "0";
+  } else {
+    alert("Operator sudah ditetapkan");
+  }
+}
+
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.firstNumber == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator == "+") {
+    result =
+      parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else if (calculator.operator == "-") {
+    result =
+      parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  } else if (calculator.operator == "*") {
+    result =
+      parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
+  } else {
+    result =
+      parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
+  }
+
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+
+  putHistory(history);
+  calculator.displayNumber = result;
+  renderHistory();
+}
+
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  button.addEventListener("click", function (e) {
+    const target = e.target;
+
+    if (target.classList.contains("clear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("operator")) {
+      handleOperation(target.innerText);
+      updateDisplay();
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+}
